2c936a9d54c4d726aeea196b71f53465e35d5bb0
xenoterror <anohinnikita537@gmail.com>
2026-08-23 17:09:44 +0300

    hw

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -17,6 +17,6 @@ router.post('/notes', createNote);
 
 router.delete('/notes/:noteId', deleteNote);
 
-router.patch('/notes/notetId', updateNote);
+router.patch('/notes/:noteId', updateNote);
 
 export default router;
