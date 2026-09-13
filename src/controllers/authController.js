d1613dfd7487cf3f539b8f2ef8b4d5d1ed4bcb0b
xenoterror <anohinnikita537@gmail.com>
2026-09-13 20:10:08 +0300

    fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,7 +9,7 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 import handlebars from 'handlebars';
 
-import { sendEMail } from '../utils/sendMail.js';
+import { sendEmail } from '../utils/sendMail.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from '../models/session.js';
 
@@ -127,7 +127,7 @@ export const requestResetEmail = async (req, res) => {
   });
 
   try {
-    await sendMail({
+    await sendEmail({
       from: process.env.SMTP_FROM,
       to: email,
       subject: 'Reset your password',
